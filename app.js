729e1c5209309fa82018e0e27944cdf28a7e7546
bon <70957955+bonniebon123@users.noreply.github.com>
2024-07-21 01:38:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/authRoutes');
+const gameRoutes = require('./routes/gameRoutes');
+const db = require('./utils/db');
 
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+app.use('/game', gameRoutes);
+
+db.connect();
+
+module.exports = app;
