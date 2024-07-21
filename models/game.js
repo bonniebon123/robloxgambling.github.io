28df1742e0d7ef898584ff3f86b374faf97274f3
bon <70957955+bonniebon123@users.noreply.github.com>
2024-07-21 01:39:46 -0500

    Update game.js

--- models/game.js
+++ models/game.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const gameSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  gameType: { type: String, required: true },
+  result: { type: String, required: true },
+  amount: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Game', gameSchema);
