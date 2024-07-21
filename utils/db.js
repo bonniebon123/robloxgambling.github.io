0f7ca4ffcf42af95e6de729e99b950699e622bad
bon <70957955+bonniebon123@users.noreply.github.com>
2024-07-21 01:39:01 -0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const connect = () => {
+  mongoose.connect('mongodb://localhost:27017/gambling', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = { connect };
