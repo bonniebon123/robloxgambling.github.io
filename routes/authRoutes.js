6b8f121d1a3944d4bfa0144ed82079dbb568cb90
bon <70957955+bonniebon123@users.noreply.github.com>
2024-07-21 01:40:54 -0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { register, login, depositRobuxHandler, withdrawRobuxHandler } = require('../controllers/authController');
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/deposit', depositRobuxHandler);
+router.post('/withdraw', withdrawRobuxHandler);
+
+module.exports = router;
 
